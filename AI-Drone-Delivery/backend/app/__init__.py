c7f08b06bd59261993340b4da0ddaff8e177b25d
Ya Fhun <phunglinhk6@gmail.com>
2026-08-17 17:12:29 +0700

    Setup: Flask project

---
+++ AI-Drone-Delivery/backend/app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from app.config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    CORS(app)
+
+    return app
