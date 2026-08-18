155f958b7c62d967f303fc454f9a25c0fe30c8bc
Quynh Anh <quynhanh223206@gmail.com>
2026-08-18 13:10:25 +0700

    CNPM-35: Complete database migration

--- AI-Drone-Delivery/backend/run.py
+++ AI-Drone-Delivery/backend/run.py
@@ -2,5 +2,9 @@ from app import create_app
 
 app = create_app()
 
+@app.route('/test-db')
+def test_db():
+    return "Ket noi PostgreSQL thanh cong!"
+
 if __name__ == '__main__':
     app.run(debug=True)
