807917e6a29b4adb4d424721bd6dead8d04461be
Ya Fhun <phunglinhk6@gmail.com>
2026-08-28 10:37:53 +0700

    Rename frontend-web/mobile app, remove obsolete src/

--- src/backend/app/models/role.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-    description = db.Column(db.String(255))
