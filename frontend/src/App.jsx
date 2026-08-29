35a82ea6f71bc5b5e4e9a6a3f9a6cce0a839da86
Bao <lebao7042000@gmail.com>
2026-08-29 16:39:17 +0700

    feat hoàn thành task 3 và task 4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 ﻿import React from 'react';
 import CustomerList from './components/CustomerList';
+import OrderList from './components/OrderList';
+import StationList from './components/StationList';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-8">
-      <div className="max-w-6xl mx-auto">
+      <div className="max-w-6xl mx-auto space-y-8">
         <CustomerList />
+        <OrderList />
+        <StationList />
       </div>
     </div>
   );
