93bb82fd339414418f8f61b18bbd2e81b4e41b08
BuiKhanhChi <chibk0480@ut.edu.vn>
2026-08-30 13:57:58 +0700

    Update: Station repository for PostGIS

--- backend/src/infrastructure/repositories/station_repository.py
+++ backend/src/infrastructure/repositories/station_repository.py
@@ -1,5 +1,6 @@
 from src.infrastructure.models.station_model import StationModel
 from src.infrastructure.databases.base import db
+from geoalchemy2 import WKTElement
 
 
 class StationRepository:
@@ -16,22 +17,46 @@ class StationRepository:
             longitude=longitude,
             capacity=capacity,
             status=status,
+            location=WKTElement(
+                f"POINT({longitude} {latitude})",
+                srid=4326
+            )
         )
+
         db.session.add(station)
         db.session.commit()
         return station
 
-    def update_station(self, station, name=None, latitude=None, longitude=None, capacity=None, status=None):
+    def update_station(
+        self,
+        station,
+        name=None,
+        latitude=None,
+        longitude=None,
+        capacity=None,
+        status=None
+    ):
         if name is not None:
             station.name = name
+
         if latitude is not None:
             station.latitude = latitude
+
         if longitude is not None:
             station.longitude = longitude
+
+        if latitude is not None or longitude is not None:
+            station.location = WKTElement(
+                f"POINT({station.longitude} {station.latitude})",
+                srid=4326
+            )
+
         if capacity is not None:
             station.capacity = capacity
+
         if status is not None:
             station.status = status
+
         db.session.commit()
         return station
 
